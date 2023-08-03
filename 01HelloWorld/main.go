56f7420be75367719043c5b85beddfa71791a128
Kumar <chandanx.kumar@intel.com>
2023-08-03 13:38:49 +0530

    Hello world

---
+++ 01HelloWorld/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Println Used for print and cover whole line.
+	fmt.Println("Hello World!")
+
+	// Print used for print msg in going line and it will cover space in terminal that muc requrired.
+	fmt.Print("Starting Golang Tutorial")
+
+	// Printf used for formatting the msg.
+	fmt.Printf("\nFirst day of golang")
+}
